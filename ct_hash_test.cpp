9df4a246dd13f219fba693b66c20ddcc160c47a0
Jaime Cornejo <38666548+jcornejope@users.noreply.github.com>
2022-10-07 18:17:26 +0100

    Ensuring some ct generations on the test code
    Minor identation/spacing

--- ct_hash_test.cpp
+++ ct_hash_test.cpp
@@ -5,24 +5,24 @@
 int main( int argc, char* argv[] )
 {
 	// Compile Time 32 const string hashing
-	char const* string = "ThisIsAString";
-	auto const string_hash = create_hash_32( string );
+	constexpr char const* string = "ThisIsAString";
+	constexpr auto const string_hash = create_hash_32( string );
 	std::cout << " sh => " << string_hash << std::endl;
 	
 	h32 arg1 = INVALID_HASH_32;
-    if ( argc > 1 )
-        arg1 = create_hash_32( argv[1] ); // Runtime 32 hashing
+	if( argc > 1 )
+		arg1 = create_hash_32( argv[1] ); // Runtime 32 hashing
 
 	int ret = -1;
 	constexpr h32 skip64Command = "DoNotTest64"_h32; // Compile Time 32 hashing
 	std::cout << arg1 << " == " << skip64Command << std::endl;
     
-	if (arg1 != skip64Command)
+	if( arg1 != skip64Command )
 	{
 		// Compile Time 64 Hashing
-		auto const str = create_hash_64("YourString");
+		constexpr auto str = create_hash_64("YourString");
 
-		switch (str)
+		switch( str )
 		{
 			case "MyString"_h64: 	ret = 0; break;
 			case "YourString"_h64: 	ret = 1; break;
@@ -36,8 +36,8 @@ int main( int argc, char* argv[] )
 	}
 
 	// Integral type hashing	
-	h32 const d32 = create_hash_32(33453242342ull);
-	h32 const d64 = create_hash_64(33453242342ull);
+	h32 const d32 = create_hash_32( 33453242342ull );
+	h64 const d64 = create_hash_64( 33453242342ull );
 	std::cout << " d32 => " << d32 << " d64 => " << d64 << std::endl;
 
 	// User defined hashing
@@ -55,5 +55,5 @@ int main( int argc, char* argv[] )
 	auto const pod_hash_2 = create_hash_64( pod_2 );
 	std::cout << " pod => " << pod_hash << " pod_2 => " << pod_hash_2 << std::endl;
 	
-    return ret;
+	return ret;
 }
